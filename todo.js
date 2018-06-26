8dac5baed8565dcc1966770d2ef23ad5b1fe90c9
VISHAL <vishal.chandra@mountblue.io>
2018-06-26 17:53:01 +0530

    item no auto update on add and delete in local storage

--- todo.js
+++ todo.js
@@ -30,32 +30,38 @@ var li;
 var inputValue;
 var t;
 var idno;
-var boolean=false;
+var boolean = false;
 var obj;
 
 function addnewElement() {
-    li = document.createElement("li");
-     inputValue = document.getElementById("myInput").value;
-     if (inputValue != '') {
-        // alert("You must write something!");
-    
-        idno = "item" + i++;
-    li.setAttribute('id',idno);
-    li.className = "listItem";
-    li.setAttribute('draggable', true);
-    li.setAttribute('ondragned', "dragEnd()");
-    li.setAttribute('ondragover', "dragOver(event)");
-    li.setAttribute('ondragstart', "dragStart(event)");
-     t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    document.getElementById("ul-items").appendChild(li);
-     var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+     var itm = JSON.parse(localStorage.getItem("todo"));
+       if(itm != null && itm.length != 0 ){
+        renderall();
+        i=itm.length;
+       }
 
-  };
+    li = document.createElement("li");
+    inputValue = document.getElementById("myInput").value;
+    if (inputValue != '') {
+        // alert("You must write something!");
+
+        idno = "item" + i++;
+        li.setAttribute('id', idno);
+        li.className = "listItem";
+        li.setAttribute('draggable', true);
+        li.setAttribute('ondragned', "dragEnd()");
+        li.setAttribute('ondragover', "dragOver(event)");
+        li.setAttribute('ondragstart', "dragStart(event)");
+        t = document.createTextNode(inputValue);
+        li.appendChild(t);
+        document.getElementById("ul-items").appendChild(li);
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        li.appendChild(span);
+
+    };
     // if (inputValue === '') {
     //     alert("You must write something!");
     // } else {
@@ -63,18 +69,18 @@ function addnewElement() {
     // }
     // document.getElementById("myInput").value = "";
 
-   
+
     // id = "item" + i++;
     // var obj=todoconstructor(id,inputValue,false);
 
-   createobject(idno,inputValue,boolean)
+    createobject(idno, inputValue, boolean)
 
     closeButton();
     // dragfunction();
 }
 
-function createobject(idno,inputValue,boolean){
-  let obj = {
+function createobject(idno, inputValue, boolean) {
+    let obj = {
         todoId: idno,
         todoText: inputValue,
         todoboolean: boolean
@@ -84,8 +90,7 @@ function createobject(idno,inputValue,boolean){
 }
 
 
-function addToLocalStorage()
-{
+function addToLocalStorage() {
     // console.log(arr);
     localStorage.setItem('todo', JSON.stringify(arr));
 }
@@ -100,66 +105,68 @@ function closeButton() {
             // div.style.display = "none";
             var div = e.target.parentNode;
             // console.log(div.id);
-           div.remove();
-           
-             var itm = JSON.parse(localStorage.getItem("todo"));
-             for (var j = 0; j <itm.length; j++) {
-               if(itm[j].todoId==div.id){
-                // console.log(div.firstElementChild.idno+" "+itm[j].todoId);
-                arr.splice(j,1);
-               
-                break;
+            div.remove();
 
-               }
-             }
-              addToLocalStorage();
+            var itm = JSON.parse(localStorage.getItem("todo"));
+            for (var j = 0; j < itm.length; j++) {
+                if (itm[j].todoId == div.id) {
+                    // console.log(div.firstElementChild.idno+" "+itm[j].todoId);
+                    arr.splice(j, 1);
+                      addToLocalStorage();
+                    break;
+
+                }
+            }
+             renderall();
+            addToLocalStorage();
            
-        }
-    }
+            
+        }  
+    }      
+
 }
-
+            
 
 
 var list = document.querySelector('ul');
 
 list.addEventListener('click', function(ev) {
-          var checkvar=ev.target;
+    var checkvar = ev.target;
     if (checkvar.tagName === 'LI') {
         checkvar.classList.toggle('checked');
         console.log(checkvar.className);
-         var itm = JSON.parse(localStorage.getItem("todo"));
-        if(checkvar.className==='listItem checked'){
-           for (var j = 0; j <itm.length; j++) {
-               if(itm[j].todoId==checkvar.id){
-                   // checkvar.todoboolean=true;
-                   //  arr.splice(j,1);
-                   // createobject(itm[j].todoId,itm[j].todoText,true)
-                   var tempobj=arr[j];
-                   tempobj.todoboolean=true;
-                   arr[j]=tempobj;
-                   addToLocalStorage(obj);
-                   break;
-        }
-      }
-        // addnewElement();
+        var itm = JSON.parse(localStorage.getItem("todo"));
+        if (checkvar.className === 'listItem checked') {
+            for (var j = 0; j < itm.length; j++) {
+                if (itm[j].todoId == checkvar.id) {
+                    // checkvar.todoboolean=true;
+                    //  arr.splice(j,1);
+                    // createobject(itm[j].todoId,itm[j].todoText,true)
+                    var tempobj = arr[j];
+                    tempobj.todoboolean = true;
+                    arr[j] = tempobj;
+                    addToLocalStorage(obj);
+                    break;
+                }
+            }
+            // addnewElement();
 
-    }
-    else{
-       for (var j = 0; j <itm.length; j++) {
-               if(itm[j].todoId==checkvar.id){
-                   // checkvar.todoboolean=true;
-                    var tempobj=arr[j];
-                   tempobj.todoboolean=false;
-                   arr[j]=tempobj;
-                   addToLocalStorage(obj);
-                   break;
-        }
-      }
-      
+        } else {
+            for (var j = 0; j < itm.length; j++) {
+                if (itm[j].todoId == checkvar.id) {
+                    // checkvar.todoboolean=true;
+                    var tempobj = arr[j];
+                    tempobj.todoboolean = false;
+                    arr[j] = tempobj;
+                    addToLocalStorage(obj);
+                    break;
+                }
+            }
 
+
+        }
+        // addToLocalStorage();
     }
-    // addToLocalStorage();
-}
 });
 
 
@@ -172,17 +179,58 @@ var desposa;
 var holdsource;
 
 function dragOver(e) {
- e.preventDefault();
-}
 
-function dragleave(e){
-   e.preventDefault();
-   e.stopPropagation();
+    var mainTag;
+    if (e.target.tagName == 'SPAN') {
+        mainTag = e.target.parentNode;
+    } else {
+        mainTag = e.target;
+    }
+    // console.log(mainTag.tagName);
+    if (isBefore(_el, e.target)) {
+        mainTag.parentNode.insertBefore(_el, mainTag);
+        // var itm = JSON.parse(localStorage.getItem("todo"));
+        // for (desposb = 0; desposb <itm.length; desposb++) {
+        //           if(itm[desposb].todoId==mainTag.id){
+        //                  var temp=arr[desposb];
+        //                  arr[desposb]= arr[sorpos]
+        //                  arr[sorpos]=arr[temp]
+        //                  sorpos--;
+        //                 // var tempobj=arr[sorpos];
+        //             //   arr.splice(despos,0,holdsource);
+        // //               arr.splice(despos,0,tempobj)
+
+        // //               // arr[sorpos]=tempobj;
+        // //               // addToLocalStorage(obj);
+        //                break;
+        //     }
+        //  } addToLocalStorage();
+
+    } else {
+        mainTag.parentNode.insertBefore(_el, mainTag.nextSibling);
+        //   var itm = JSON.parse(localStorage.getItem("todo"));
+        // for (desposa = 0; desposa <itm.length; desposa++) {
+        //           if(itm[desposa].todoId==mainTag.id){
+        //                  var temp=arr[desposa];
+        //                  arr[desposa]= arr[sorpos]
+        //                  arr[sorpos]=arr[temp]
+
+        //                 // var tempobj=arr[sorpos];
+        //             //   arr.splice(despos,0,holdsource);
+        // //               arr.splice(despos,0,tempobj)
+
+        // //               // arr[sorpos]=tempobj;
+        // //               // addToLocalStorage(obj);
+        //                break;
+        //     }
+        //  }
+
+    }
 }
 
 function dragEnd() {
- //   _el = null;
-     addToLocalStorage();
+    //   _el = null;
+    addToLocalStorage();
 }
 
 function dragStart(e) {
@@ -190,18 +238,19 @@ function dragStart(e) {
     // e.dataTransfer.setData("text/plain", null);
     _el = e.target;
     // console.log(e.target);
-      var itm = JSON.parse(localStorage.getItem("todo"));
-     for (sorpos = 0; sorpos <itm.length; sorpos++) {
-               if(itm[sorpos].todoId==_el.id){
-                   // checkvar.todoboolean=true;
-                   //  var tempobj=arr[sorpos];
-                   holdsource=arr[sorpos];
-                  // arr.splice(sorpos,1)
-                   // arr[sorpos]=tempobj;
-                   // addToLocalStorage(obj);
-                   break;
+    var itm = JSON.parse(localStorage.getItem("todo"));
+    for (sorpos = 0; sorpos < itm.length; sorpos++) {
+        if (itm[sorpos].todoId == _el.id) {
+            // checkvar.todoboolean=true;
+            //  var tempobj=arr[sorpos];
+            holdsource = arr[sorpos];
+            // arr.splice(sorpos,1)
+            // arr[sorpos]=tempobj;
+            // addToLocalStorage(obj);
+            break;
         }
-      }addToLocalStorage();
+    }
+    addToLocalStorage();
 
 }
 
@@ -213,10 +262,34 @@ function isBefore(el1, el2) {
     return false;
 }
 
-// localStorage.autoalert = "false";
-// window.alert(localStorage.getItem("autoalert"));
-// $("input[name='activateAutoAlert']").click(function() {
-//     var activateAutoAlert = $("input[name='activateAutoAlert']").is(":checked") ? "true" : "false";
-//     localStorage.autoalert = activateAutoAlert;
-//     alert(localStorage.autoalert);
-// });
+// window.onload=function on;
+
+function renderall() {
+    var p = 0; 
+    var temparr=[];
+    
+    var itm = JSON.parse(localStorage.getItem("todo"));
+    
+        for (var j = 0; j < itm.length; j++) 
+            {   
+              var objnew = {
+                   todoId: "item"+j,
+                   todoText: itm[j].todoText,
+                   todoboolean: itm[j].todoboolean
+                };
+                temparr.push(objnew) 
+            }
+        arr=temparr;
+
+    }
+
+    
+  
+
+    // localStorage.autoalert = "false";
+    // window.alert(localStorage.getItem("autoalert"));
+    // $("input[name='activateAutoAlert']").click(function() {
+    //     var activateAutoAlert = $("input[name='activateAutoAlert']").is(":checked") ? "true" : "false";
+    //     localStorage.autoalert = activateAutoAlert;
+    //     alert(localStorage.autoalert);
+    // });
